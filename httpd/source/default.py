710cd8ab3559d168bc2db695afd36511b870ca83
streppuiu <streppuiu@gmail.com>
2014-06-01 19:06:59 +0000

    Converted to system.d service + httpd.conf changes (ServerLimit & MaxClients)

--- httpd/source/default.py
+++ httpd/source/default.py
@@ -17,26 +17,3 @@
 # the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
 # http://www.gnu.org/copyleft/gpl.html
 ################################################################################
-
-import xbmc, time, os, subprocess, xbmcaddon
-
-__scriptname__ = "httpd service"
-__author__ = "OpenELEC"
-__url__ = "http://www.openelec.tv"
-__settings__ = xbmcaddon.Addon(id='service.web.httpd')
-__cwd__ = __settings__.getAddonInfo('path')
-__start__ = xbmc.translatePath( os.path.join( __cwd__, 'bin', "httpd.start") )
-__stop__ = xbmc.translatePath( os.path.join( __cwd__, 'bin', "httpd.stop") )
-
-# make binary files executable in addon bin folder
-subprocess.Popen("chmod -R a+x " + __cwd__ + "/bin/*" , shell=True, close_fds=True)
-
-# run start script
-subprocess.Popen(__start__, shell=True, close_fds=True)
-
-# wait for the stop request
-while (not xbmc.abortRequested):
-   time.sleep(0.5)
-
-# stop the program
-subprocess.Popen(__stop__, shell=True, close_fds=True)
